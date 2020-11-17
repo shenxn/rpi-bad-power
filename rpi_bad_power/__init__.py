47c137335d2ba99e7a8cc8b18170a773d6d8ee9b
Stefan Agner <stefan@agner.ch>
2020-11-17 01:11:14 +0100

    Find correct hwmon device if multiple are present (fixes #1) (#2)

--- rpi_bad_power/__init__.py
+++ rpi_bad_power/__init__.py
@@ -7,7 +7,10 @@ import os
 
 _LOGGER = logging.getLogger(__name__)
 
-SYSFILE = "/sys/class/hwmon/hwmon0/in0_lcrit_alarm"
+HWMON_NAME = "rpi_volt"
+
+SYSFILE_HWMON_DIR = "/sys/class/hwmon"
+SYSFILE_HWMON_FILE = "in0_lcrit_alarm"
 SYSFILE_LEGACY = "/sys/devices/platform/soc/soc:firmware/get_throttled"
 
 UNDERVOLTAGE_STICKY_BIT = 1 << 16
@@ -15,11 +18,21 @@ UNDERVOLTAGE_STICKY_BIT = 1 << 16
 DESCRIPTION_NORMALIZED = "Voltage normalized. Everything is working as intended."
 DESCRIPTION_UNDER_VOLTAGE = "Under-voltage was detected. Consider getting a uninterruptible power supply for your Raspberry Pi."
 
+def get_rpi_volt_hwmon():
+    """Find rpi_volt hwmon device"""
+    for hwmon in os.listdir(SYSFILE_HWMON_DIR):
+        name_file = os.path.join(SYSFILE_HWMON_DIR, hwmon, "name")
+        if os.path.isfile(name_file):
+            hwmon_name = open(name_file).read().strip()
+            if hwmon_name == HWMON_NAME:
+                return os.path.join(SYSFILE_HWMON_DIR, hwmon)
+    return None
 
 def new_under_voltage():
     """Create new UnderVoltage object."""
-    if os.path.isfile(SYSFILE):
-        return UnderVoltage()
+    hwmon = get_rpi_volt_hwmon()
+    if hwmon:
+        return UnderVoltage(hwmon)
     if os.path.isfile(SYSFILE_LEGACY):  # support older kernel
         return UnderVoltageLegacy()
     return None
@@ -28,10 +41,13 @@ def new_under_voltage():
 class UnderVoltage:
     """Read under voltage status."""
 
+    def __init__(self, hwmon):
+        self._hwmon = hwmon
+
     def get(self):
         """Get under voltage status."""
         # Use new hwmon entry
-        bit = open(SYSFILE).read()[:-1]
+        bit = open(os.path.join(self._hwmon, SYSFILE_HWMON_FILE)).read()[:-1]
         _LOGGER.debug("Get under voltage status: %s", bit)
         return bit == "1"
 
