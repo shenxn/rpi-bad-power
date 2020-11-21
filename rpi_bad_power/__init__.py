f89deb602735cb7edea3669ae7a8f72dde501231
Xiaonan Shen <s@sxn.dev>
2020-11-21 02:04:00 -0800

    Add code style checks (#4)
    * Add pre-commit
    
    * Add mypy
    
    * Add pylint
    
    * Add code style workflow
    
    * Fix codeql workflow
    
    * Fix code cov
    
    * Fix bandit

--- rpi_bad_power/__init__.py
+++ rpi_bad_power/__init__.py
@@ -1,9 +1,11 @@
 """
-A sensor platform which detects underruns and capped status from the official Raspberry Pi Kernel.
+A library reading under voltage bit from the official Raspberry Pi Kernel.
+
 Minimal Kernel needed is 4.14+
 """
 import logging
 import os
+from typing import Optional, Text
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -15,11 +17,9 @@ SYSFILE_LEGACY = "/sys/devices/platform/soc/soc:firmware/get_throttled"
 
 UNDERVOLTAGE_STICKY_BIT = 1 << 16
 
-DESCRIPTION_NORMALIZED = "Voltage normalized. Everything is working as intended."
-DESCRIPTION_UNDER_VOLTAGE = "Under-voltage was detected. Consider getting a uninterruptible power supply for your Raspberry Pi."
 
-def get_rpi_volt_hwmon():
-    """Find rpi_volt hwmon device"""
+def get_rpi_volt_hwmon() -> Optional[Text]:
+    """Find rpi_volt hwmon device."""
     try:
         hwmons = os.listdir(SYSFILE_HWMON_DIR)
     except FileNotFoundError:
@@ -35,26 +35,22 @@ def get_rpi_volt_hwmon():
 
     return None
 
-def new_under_voltage():
-    """Create new UnderVoltage object."""
-    hwmon = get_rpi_volt_hwmon()
-    if hwmon:
-        return UnderVoltageNew(hwmon)
-    if os.path.isfile(SYSFILE_LEGACY):  # support older kernel
-        return UnderVoltageLegacy()
-    return None
-
 
 class UnderVoltage:
     """Read under voltage status."""
 
+    def get(self) -> bool:
+        """Get under voltage status."""
+
+
 class UnderVoltageNew(UnderVoltage):
     """Read under voltage status from new entry."""
 
-    def __init__(self, hwmon):
+    def __init__(self, hwmon: Text):
+        """Initialize the under voltage class."""
         self._hwmon = hwmon
 
-    def get(self):
+    def get(self) -> bool:
         """Get under voltage status."""
         # Use new hwmon entry
         with open(os.path.join(self._hwmon, SYSFILE_HWMON_FILE)) as file:
@@ -66,13 +62,22 @@ class UnderVoltageNew(UnderVoltage):
 class UnderVoltageLegacy(UnderVoltage):
     """Read under voltage status from legacy entry."""
 
-    def get(self):
+    def get(self) -> bool:
         """Get under voltage status."""
         # Using legacy get_throttled entry
         with open(SYSFILE_LEGACY) as file:
             throttled = file.read()[:-1]
         _LOGGER.debug("Get throttled value: %s", throttled)
         return (
-            int(throttled, base=16) & UNDERVOLTAGE_STICKY_BIT
-            == UNDERVOLTAGE_STICKY_BIT
+            int(throttled, base=16) & UNDERVOLTAGE_STICKY_BIT == UNDERVOLTAGE_STICKY_BIT
         )
+
+
+def new_under_voltage() -> Optional[UnderVoltage]:
+    """Create new UnderVoltage object."""
+    hwmon = get_rpi_volt_hwmon()
+    if hwmon:
+        return UnderVoltageNew(hwmon)
+    if os.path.isfile(SYSFILE_LEGACY):  # support older kernel
+        return UnderVoltageLegacy()
+    return None
--- test_rpi_bad_power.py
+++ test_rpi_bad_power.py
@@ -1,146 +1,185 @@
-"""
-Testcases for rpi bad power
-"""
+"""Testcases for rpi bad power."""
 
-from unittest.mock import patch, MagicMock
-from rpi_bad_power import new_under_voltage, SYSFILE_HWMON_DIR, SYSFILE_LEGACY, UnderVoltageLegacy, UnderVoltageNew
+from typing import Any, List, Mapping, Text
+from unittest.mock import MagicMock, patch
+
+from rpi_bad_power import (
+    SYSFILE_HWMON_DIR,
+    SYSFILE_LEGACY,
+    UnderVoltageLegacy,
+    UnderVoltageNew,
+    new_under_voltage,
+)
 
 
 class MockFile:
     """Mocked file."""
-    def __init__(self, content):
+
+    def __init__(self, content: Text):
+        """Initialize the mocked file."""
         self.read = MagicMock(return_value=f"{content}\n")
         self.close = MagicMock()
 
-    def __enter__(self):
-        return self
 
-    def __exit__(self, exc_type, exc_value, tb):
-        self.close()
+class PatchFile:
+    """Patch a file."""
+
+    def __init__(self, content: Text):
+        """Initialize the mocked file."""
+        self.file = MockFile(content)
+
+    def __enter__(self) -> MockFile:
+        """Enter a with statement doing nothing."""
+        return self.file
+
+    def __exit__(self, *exc_info: Any) -> bool:
+        """Exit a with statement by closing the file."""
+        self.file.close()
         return True
 
 
 class MockSysFiles:
     """Mocking the system files."""
-    def __init__(self, multi, new, legacy, noHwmon, isUnderVoltage):
+
+    def __init__(
+        self,
+        multi: bool,
+        new: bool,
+        legacy: bool,
+        no_hwmon: bool,
+        is_under_voltage: bool,
+    ):
+        """Initialize the mocked system files."""
         self.multi = multi  # multiple entries
         self.new = new  # single new entry
         self.legacy = legacy  # legacy entry
-        self.noHwmon = noHwmon  # hwmon dir does not exist
-        self.isUnderVoltage = isUnderVoltage
+        self.no_hwmon = no_hwmon  # hwmon dir does not exist
+        self.is_under_voltage = is_under_voltage
 
         self.listdir = MagicMock(side_effect=self._listdir)
         self.open = MagicMock(side_effect=self._open)
         self.isfile = MagicMock(side_effect=self._isfile)
 
-        self.openedFiles = []
-        self.files = {}
+        self.opened_files: List[MockFile] = []
+        self.files: Mapping[Text, Text] = {}
         if multi:
             self.files[f"{SYSFILE_HWMON_DIR}/hwmon0/name"] = "cpu_thermal"
             self.files[f"{SYSFILE_HWMON_DIR}/hwmon2/name"] = "rpi_volt"
-            self.files[f"{SYSFILE_HWMON_DIR}/hwmon2/in0_lcrit_alarm"] = "1" if isUnderVoltage else "0"
+            self.files[f"{SYSFILE_HWMON_DIR}/hwmon2/in0_lcrit_alarm"] = (
+                "1" if is_under_voltage else "0"
+            )
         elif new:
             self.files[f"{SYSFILE_HWMON_DIR}/hwmon0/name"] = "rpi_volt"
-            self.files[f"{SYSFILE_HWMON_DIR}/hwmon0/in0_lcrit_alarm"] = "1" if isUnderVoltage else "0"
+            self.files[f"{SYSFILE_HWMON_DIR}/hwmon0/in0_lcrit_alarm"] = (
+                "1" if is_under_voltage else "0"
+            )
         elif legacy:
-            self.files[SYSFILE_LEGACY] = "50005" if self.isUnderVoltage else "0"
+            self.files[SYSFILE_LEGACY] = "50005" if self.is_under_voltage else "0"
 
-    def _listdir(self, path):
+    def _listdir(self, path: Text) -> List[Text]:
         assert path == SYSFILE_HWMON_DIR
-        if self.noHwmon:
+        if self.no_hwmon:
             raise FileNotFoundError()
         if self.multi:
             return ["hwmon0", "hwmon1", "hwmon2"]
-        elif self.new:
+        if self.new:
             return ["hwmon0"]
-        else:
-            return []
+        return []
 
-    def _open(self, path):
+    def _open(self, path: Text) -> PatchFile:
         try:
-            file = MockFile(self.files[path])
+            patch_file = PatchFile(self.files[path])
         except KeyError:
-            raise FileNotFoundError()
-        self.openedFiles.append(file)
-        return file
+            raise FileNotFoundError()  # pylint: disable=raise-missing-from
+        self.opened_files.append(patch_file.file)
+        return patch_file
 
-    def _isfile(self, path):
+    def _isfile(self, path: Text) -> bool:
         return path in self.files
 
-    def assertAllFilesClosed(self):
-        for file in self.openedFiles:
+    def assert_all_files_closed(self) -> None:
+        """Assert all opened files are closed."""
+        for file in self.opened_files:
             file.close.assert_called_once()
 
 
 class PatchSysFiles:
     """Patch the system files."""
-    def __init__(self, multi=False, new=False, legacy=False, noHwmn=False, isUnderVoltage=False):
-        self.mock = MockSysFiles(multi, new, legacy, noHwmn, isUnderVoltage)
-        self.listdir_patch = None
-        self.open_patch = None
-        self.isfile_patch = None
-    
-    def __enter__(self):
+
+    def __init__(
+        self,
+        multi: bool = False,
+        new: bool = False,
+        legacy: bool = False,
+        no_hwmon: bool = False,
+        is_under_voltage: bool = False,
+    ):
+        """Initialize the patch helper class."""
+        self.mock = MockSysFiles(multi, new, legacy, no_hwmon, is_under_voltage)
         self.listdir_patch = patch("rpi_bad_power.os.listdir", self.mock.listdir)
         self.open_patch = patch("rpi_bad_power.open", self.mock.open)
         self.isfile_patch = patch("rpi_bad_power.os.path.isfile", self.mock.isfile)
+
+    def __enter__(self) -> MockSysFiles:
+        """Enter the with statement by patching functions."""
         self.listdir_patch.__enter__()
         self.open_patch.__enter__()
         self.isfile_patch.__enter__()
         return self.mock
-    
-    def __exit__(self, exc_type, exc_value, tb):
-        if exc_type is not None:
+
+    def __exit__(self, *exc_info: Any) -> bool:
+        """Exit the with statement by un-patching functions."""
+        if exc_info[0] is not None:
             return False
-        self.listdir_patch.__exit__(exc_type, exc_value, tb)
-        self.open_patch.__exit__(exc_type, exc_value, tb)
-        self.isfile_patch.__exit__(exc_type, exc_value, tb)
-        self.mock.assertAllFilesClosed()
+        self.listdir_patch.__exit__(*exc_info)
+        self.open_patch.__exit__(*exc_info)
+        self.isfile_patch.__exit__(*exc_info)
+        self.mock.assert_all_files_closed()
         return True
 
 
-def test_non_rpi():
+def test_non_rpi() -> None:
     """Test running on non raspberry pi environment."""
-    with PatchSysFiles() as mockSysFiles:
+    with PatchSysFiles() as mock_sys_files:
         assert new_under_voltage() is None
-    mockSysFiles.listdir.assert_called_once_with(SYSFILE_HWMON_DIR)
+    mock_sys_files.listdir.assert_called_once_with(SYSFILE_HWMON_DIR)
 
 
-def test_non_rpi():
+def test_no_hwmon() -> None:
     """Test running on a system without hwmon directory."""
-    with PatchSysFiles(noHwmn=True):
+    with PatchSysFiles(no_hwmon=True):
         assert new_under_voltage() is None
 
 
-def test_multi():
+def test_multi() -> None:
     """Test running on a rpi kernel with multiple hwmon entries."""
     with PatchSysFiles(True):
-        underVoltage = new_under_voltage()
-        assert isinstance(underVoltage, UnderVoltageNew)
-        assert underVoltage.get() is False
-    
-    with PatchSysFiles(True, isUnderVoltage=True):
-        assert new_under_voltage().get() is True
+        under_voltage = new_under_voltage()
+        assert isinstance(under_voltage, UnderVoltageNew)
+        assert under_voltage.get() is False
+
+    with PatchSysFiles(True, is_under_voltage=True):
+        assert new_under_voltage().get() is True  # type: ignore
 
 
-def test_new():
+def test_new() -> None:
     """Test running on a rpi kernel with one new hwmon entry."""
     with PatchSysFiles(new=True):
-        underVoltage = new_under_voltage()
-        assert isinstance(underVoltage, UnderVoltageNew)
-        assert underVoltage.get() is False
+        under_voltage = new_under_voltage()
+        assert isinstance(under_voltage, UnderVoltageNew)
+        assert under_voltage.get() is False
 
-    with PatchSysFiles(new=True, isUnderVoltage=True):
-        assert new_under_voltage().get() is True
+    with PatchSysFiles(new=True, is_under_voltage=True):
+        assert new_under_voltage().get() is True  # type: ignore
 
 
-def test_legacy():
+def test_legacy() -> None:
     """Test running on a legacy rpi kernel."""
-    with PatchSysFiles(legacy=True) as mockSysFiles:
-        underVoltage = new_under_voltage()
-        assert isinstance(underVoltage, UnderVoltageLegacy)
-        assert underVoltage.get() is False
-    mockSysFiles.isfile.assert_called_once_with(SYSFILE_LEGACY)
+    with PatchSysFiles(legacy=True) as mock_sys_files:
+        under_voltage = new_under_voltage()
+        assert isinstance(under_voltage, UnderVoltageLegacy)
+        assert under_voltage.get() is False
+    mock_sys_files.isfile.assert_called_once_with(SYSFILE_LEGACY)
 
-    with PatchSysFiles(legacy=True, isUnderVoltage=True):
-        assert new_under_voltage().get() is True
+    with PatchSysFiles(legacy=True, is_under_voltage=True):
+        assert new_under_voltage().get() is True  # type: ignore
